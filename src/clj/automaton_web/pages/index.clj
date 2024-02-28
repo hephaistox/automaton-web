db770bfbb2c57abc8f9832d342466cf5752f8ff8
kaspazza <kaspazza@gmail.com>
2024-02-28 08:49:18 +0100

    Add meta tags needed for for proper page preview in socials

--- src/clj/automaton_web/pages/index.clj
+++ src/clj/automaton_web/pages/index.clj
@@ -5,20 +5,24 @@
 
 (defn build
   "Build a webpage header"
-  [{:keys [header-elements app-name]} & body]
-  (let
-    [meta-description
-     [:meta
-      {:name "description"
-       :content
-       "With over two decades of expertise in supply chain and IT, working with many industries, we have the tools and knowledge to help you grow!"}]
-     icon [:link {:rel "icon"
-                  :href "favicon.ico"}]
-     css [:link {:type "text/css"
-                 :rel "stylesheet"
-                 :href "/css/compiled/styles.css"}]
-     title [:title app-name]
-     head-elements
-     [meta-description icon css (for [el header-elements] el) title]
-     body-elements (merge [(anti-forgery/anti-forgery-html-token)] body)]
+  [{:keys [header-elements meta-tags]} & body]
+  (let [{:keys [image description title]} meta-tags
+        meta-description [:meta {:name "description"
+                                 :content description}]
+        meta-image [:meta {:name "image"
+                           :content image}]
+        meta-title [:meta {:name "title"
+                           :content title}]
+        icon [:link {:rel "icon"
+                     :href "favicon.ico"}]
+        css [:link {:type "text/css"
+                    :rel "stylesheet"
+                    :href "/css/compiled/styles.css"}]
+        head-elements [meta-title
+                       meta-image
+                       meta-description
+                       icon
+                       css
+                       (for [el header-elements] el)]
+        body-elements (merge [(anti-forgery/anti-forgery-html-token)] body)]
     (str (web-hiccup/html-core head-elements body-elements))))
