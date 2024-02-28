15ec88d17296337c391bd1104a5ebfb23ecabfd7
kaspazza <kaspazza@gmail.com>
2024-02-28 10:45:52 +0100

    1.1.0-2-la

--- src/clj/automaton_web/pages/index.clj
+++ src/clj/automaton_web/pages/index.clj
@@ -6,19 +6,25 @@
 (defn build
   "Build a webpage header"
   [{:keys [header-elements meta-tags]} & body]
-  (let [{:keys [image description title]} meta-tags
-        meta-description [:meta {:name "description"
+  (let [{:keys [image description title type url]} meta-tags
+        meta-type [:meta {:name "og:type"
+                          :content type}]
+        meta-description [:meta {:name "og:description"
                                  :content description}]
-        meta-image [:meta {:name "image"
+        meta-image [:meta {:name "og:image"
                            :content image}]
-        meta-title [:meta {:name "title"
+        meta-title [:meta {:name "og:title"
                            :content title}]
+        meta-url [:meta {:name "og:url"
+                         :content url}]
         icon [:link {:rel "icon"
                      :href "favicon.ico"}]
         css [:link {:type "text/css"
                     :rel "stylesheet"
                     :href "/css/compiled/styles.css"}]
         head-elements [meta-title
+                       meta-type
+                       meta-url
                        meta-image
                        meta-description
                        icon
