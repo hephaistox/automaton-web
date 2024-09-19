319f1a849fe8bb5d207e6b3d88dc0c93d450630d
kaspazza <kaspazza@gmail.com>
2024-09-19 17:30:13 +0200

    automatic commit

---
+++ src/cljs/automaton_web/utils/date.cljs
@@ -0,0 +1,6 @@
+(ns automaton-web.utils.date
+  "Utility function for date management"
+  (:require
+   [cljs-time.core :as cljs-time]))
+
+(defn this-year ([date] (cljs-time/year date)) ([] (this-year (cljs-time/now))))
