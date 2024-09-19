db70f91128bfc0fd80341e860b8f880deea74c49
kaspazza <kaspazza@gmail.com>
2024-09-19 21:22:26 +0200

    automatic commit

---
+++ src/cljs/automaton_web/log/tracking/error_tracking.cljs
@@ -0,0 +1,13 @@
+(ns automaton-web.log.tracking.error-tracking
+  "Web error tracking related code."
+  (:require
+   [automaton-web.log.tracking.sentry :as web-log-tracking-sentry]))
+
+(defn init-error-tracking!
+  "Initializes error tracking, currently focused on sentry."
+  [{:keys [dsn traced-website env]}]
+  (when-not dsn (prn "dsn is missing in init-error-tracking!"))
+  (when-not env (prn "env is missing in init-error-tracking!"))
+  (web-log-tracking-sentry/init-sentry! {:dsn dsn
+                                         :traced-website traced-website
+                                         :env env}))
