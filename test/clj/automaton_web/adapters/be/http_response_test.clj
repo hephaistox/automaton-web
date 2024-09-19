8bdf7a3161e4ce861daaa81fd48bd77e01451894
kaspazza <kaspazza@gmail.com>
2024-09-19 13:35:08 +0200

    automatic commit

---
+++ test/clj/automaton_web/adapters/be/http_response_test.clj
@@ -0,0 +1,37 @@
+(ns automaton-web.adapters.be.http-response-test
+  (:require
+   [automaton-web.adapters.be.http-response :as sut]
+   [clojure.test                            :refer [deftest is testing]]))
+
+(deftest default-handlers-test
+  (testing "with default http responses"
+    (testing "test ok is returning 200"
+      (is (= {:status 200
+              :headers {}
+              :body "test"}
+             (sut/ok "test"))))
+    (testing "text ok is returning headers that are passed"
+      (is (= {:status 200
+              :headers {"content-type" "text"}
+              :body "test"}
+             (sut/ok {"content-type" "text"} "test"))))
+    (testing "test no-content is returning 204"
+      (is (= {:status 204
+              :headers {}
+              :body nil}
+             (sut/no-content))))
+    (testing "test not-found handler is called and returning 404 status"
+      (is (= {:status 404
+              :headers {}
+              :body "not-found"}
+             (sut/not-found "not-found"))))
+    (testing "test method-not-allowed handler is called and returning 405 status"
+      (is (= {:status 405
+              :headers {}
+              :body "method-not-allowed"}
+             (sut/method-not-allowed "method-not-allowed"))))
+    (testing "test not-acceptable handler is called and returning 404 status"
+      (is (= {:status 406
+              :headers {}
+              :body "not-acceptable"}
+             (sut/not-acceptable "not-acceptable"))))))
---
+++ test/cljs/automaton_web/fe/router/reitit_test.cljs
@@ -0,0 +1,24 @@
+(ns automaton-web.fe.router.reitit-test
+  (:require
+   [automaton-web.fe.router        :as web-fe-router]
+   [automaton-web.fe.router.reitit :as sut]
+   [cljs.test                      :refer          [deftest is testing]
+                                   :include-macros true]))
+
+(def reitit-router
+  (sut/make-reitit-router ["/foo"
+                           {:name ::foo
+                            :panel-id ::get-fe-foo}]
+                          (constantly {})))
+
+(deftest make-reitit-router-test
+  (testing "testing matching route"
+    (is (= ::get-fe-foo
+           (->> "/foo"
+                (web-fe-router/match-from-url reitit-router)
+                (web-fe-router/panel-id reitit-router)))))
+  (testing "testing not matching route"
+    (is (= :panels/not-found
+           (->> "/bar"
+                (web-fe-router/match-from-url reitit-router)
+                (web-fe-router/panel-id reitit-router))))))
