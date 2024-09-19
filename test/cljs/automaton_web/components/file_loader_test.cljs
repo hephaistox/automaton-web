80d3a1b8d972e8c5d2e6b2b362903226727c8b6b
kaspazza <kaspazza@gmail.com>
2024-09-19 13:29:59 +0200

    automatic commit

--- test/cljs/automaton_web/components/file_loader_test.cljs
+++
@@ -1,9 +0,0 @@
-(ns automaton-web.components.file-loader-test
-  (:require
-   [automaton-web.components.file-loader :as sut]
-   [cljs.test                            :refer          [deftest is testing]
-                                         :include-macros true]))
-
-(deftest component
-  (testing "No parameter raises an error"
-    (is (= [:div "Error in the component parameters"] (sut/component {})))))
