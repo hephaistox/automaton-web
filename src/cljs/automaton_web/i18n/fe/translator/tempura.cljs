8bf3ee0aacea0888a31b741296d302a139521f87
kaspazza <kaspazza@gmail.com>
2024-09-20 15:02:25 +0200

    automatic commit

---
+++ src/cljs/automaton_web/i18n/fe/translator/tempura.cljs
@@ -0,0 +1,61 @@
+(ns automaton-web.i18n.fe.translator.tempura
+  "Tempura implementation for frontend translation
+  Implementing `fe-translator/FeTranslator`"
+  (:require
+   [automaton-core.i18n.translator.tempura :as tempura-translator]
+   [automaton-core.log                     :as core-log]
+   [automaton-web.adapters.fe.cookies      :as fe-cookies]
+   [automaton-web.adapters.fe.url          :as fe-url]
+   [automaton-web.events-proxy             :as web-events-proxy]
+   [automaton-web.events.subs              :as web-subs]
+   [automaton-web.i18n.dict.resources]
+   [automaton-web.i18n.dict.text]
+   [automaton-web.i18n.fe.translator       :as fe-translator]
+   [taoensso.tempura                       :as tempura]))
+
+(defrecord FeTempuraTranslator [opts main-langs ui-str-to-id]
+  fe-translator/FeTranslator
+    (default-languages [_] main-langs)
+    (translate [_ tr-id resources]
+      (let [lang (some-> (web-events-proxy/subscribe [::web-subs/lang])
+                         deref)
+            translated-text (tempura/tr opts
+                                        (vec (concat (when (keyword? lang) [lang]) main-langs))
+                                        [tr-id]
+                                        resources)]
+        (core-log/trace "Translate key `"
+                        tr-id
+                        "`,with locales `"
+                        (vec (concat (when (keyword? lang) [lang]) main-langs))
+                        "`, -> `"
+                        translated-text
+                        "`")
+        translated-text))
+    (init-lang [this]
+      (fe-translator/-init-lang this
+                                (-> (fe-url/current-url)
+                                    fe-url/lang-in-url-par
+                                    ui-str-to-id)
+                                (delay (-> "lang"
+                                           fe-cookies/get-cookie-val
+                                           ui-str-to-id))
+                                main-langs))
+    (lang [this] (fe-translator/-lang this)))
+
+(defn make-fe-tempura-translator
+  "Create a frontend tempura translator
+  The language will be based on the `::web-subs/lang`
+    Params:
+  * `dicts` ordered list of dictionaries, the last is lower priority. The following dictionaries are appended, with lower priorities than yours so you can override them:
+      * default text for `automaton-web` components
+      * missing keys of tempura
+  * `ui-str-to-id`
+  * `main-langs` languages defaulted by tempura if not found "
+  [main-langs ui-str-to-id & dicts]
+  ;;Adding web-dict here allows to use that keyword in cust apps
+  (->FeTempuraTranslator (apply tempura-translator/create-opts
+                                automaton-web.i18n.dict.text/dict
+                                automaton-web.i18n.dict.resources/dict
+                                dicts)
+                         main-langs
+                         ui-str-to-id))
---
+++ test/cljc/automaton_web/routes_test.cljc
@@ -0,0 +1,31 @@
+(ns automaton-web.routes-test
+  (:require
+   [automaton-web.routes :as sut]
+   #?(:clj [clojure.test :refer [deftest is testing]]
+      :cljs [cljs.test :refer [deftest is testing] :include-macros true])))
+
+(deftest parse-route-elt-test
+  (testing "Strings are preserved" (is (= "foo" (sut/parse-routes :be "foo"))))
+  (testing "If be or fe data isn't a map, so copy it directly (usefull to pass handlers directly)"
+    (is (= 'clojure.print
+           (sut/parse-routes :be
+                             {:name :foo
+                              :be 'clojure.print}))))
+  (testing "If be or fe are a map, copy it with `:name` attribute appended"
+    (is (= {:name ::root
+            :get clojure.core/print}
+           (sut/parse-routes :be
+                             {:name ::root
+                              :be {:get :html-page/index}
+                              :fe {:panel-id :panels/home}}
+                             {:html-page/index clojure.core/print})))
+    (is (= {:name ::root
+            :panel-id :panels/home}
+           (sut/parse-routes :fe
+                             {:name ::root
+                              :be {:page-id :html-page/index}
+                              :fe {:panel-id :panels/home}}))))
+  (testing "Vectors values are treated" (is (= ["foo" "bar"] (sut/parse-routes :be ["foo" "bar"]))))
+  (testing "Registry replace keywords"
+    (is (= ["" clojure.core/last]
+           (sut/parse-routes :be ["" {:be :foo}] {:foo clojure.core/last})))))
